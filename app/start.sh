94ee2995ebe1d671e1683e175f4fa13876700f8c
Jason Fry <jason@jasonfry.co.uk>
2018-09-10 12:16:40 +0100

    timezone

---
+++ app/start.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+# Default to UTC if no TIMEZONE env variable is set
+echo "Setting time zone to ${TIMEZONE=UTC}"
+# This only works on Debian-based images
+echo "${TIMEZONE}" > /etc/timezone
+dpkg-reconfigure tzdata
+
+
+python app.py
